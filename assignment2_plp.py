25ce672a6fe46fe751f6a9f87fa1a43bfbb5b39d
mwarain <161334662+mwarain@users.noreply.github.com>
2026-02-10 04:27:41 +0300

    Add files via upload

---
+++ assignment2_plp.py
@@ -0,0 +1,26 @@
+my_list = []
+print(my_list)
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+my_list.insert(1, 15)
+print(my_list)
+
+another_list = [50, 60, 70]
+my_list.extend(another_list)
+print(my_list)
+
+my_list.remove(70)
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+print(my_list.index(30))
+
+
+
